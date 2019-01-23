1d741400098daaaa6220c5f69be8d158cc7e5839
Michelle Alarie <alarie.michelle@gmail.com>
2019-01-22 21:26:24 -0500

    Make option for decimal point params

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,8 @@
 """
 Testing addition in calculator.
 """
+import pytest
+
 from calculator import add
 
 def test_two_plus_two():
@@ -26,3 +28,16 @@ def test_one_two_three():
     If given 1, 2 and 3, 6 should be returned
     """
     assert add(1, 2, 3) == 6
+
+def test_negative_values():
+    """
+    If given
+    """
+    assert add(-1, -1, -1, -1, -1) == -5
+
+def test_decimal_values():
+    """
+    Asserts that 0.1, 0.1, and 0.1 equals 0.3
+    """
+    # import pytest lib so we could use the approx function instead of round
+    assert add(0.1,0.1,0.1) == pytest.approx(0.3)
