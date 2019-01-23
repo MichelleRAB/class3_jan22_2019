74f73287f5efb76927e7984b31d15b7905c6e469
Michelle Alarie <alarie.michelle@gmail.com>
2019-01-22 20:58:28 -0500

    Make parameters optional for add function

--- calculator.py
+++ calculator.py
@@ -4,4 +4,4 @@ A calculator library.
 
 def add(value1, value2):
     # return 6: this would give us an error
-    return 4
+    return value1 + value2
--- tests/test_add.py
+++ tests/test_add.py
@@ -8,3 +8,9 @@ def test_two_plus_two():
     If the number 2 is given as a param, 4 should be returned.
     """
     assert add(2, 2) == 4
+
+def test_three_and_three():
+    """
+    If given 3 and 3, 6 should be returned
+    """
+    assert add(3, 3) == 6
