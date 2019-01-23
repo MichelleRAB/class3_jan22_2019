e7f8c51d929d8338d99edf74b9cda6aa8cb3c9ad
Michelle Alarie <alarie.michelle@gmail.com>
2019-01-22 20:39:28 -0500

    Built an add function that sums 2 and 2

--- tests/tdd_example.py
+++ tests/tdd_example.py
@@ -1,9 +1,17 @@
 
 """
-An example test module
-
+An example test module.
+Steps to tdd:
+1. Create a tests folder
+2. Create an init file
+3. Create a file
 """
 def test_the_truth():
     """Assert the turh!"""
     # pass
     assert False
+
+def test_the_math():
+    num = 3
+    if num > 0:
+        raise Exception("Nope")
